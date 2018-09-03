fda437f8a06110f202d87b63b00fc8ea7289ae09
Nenivar <nenivar@gmail.com>
2018-09-03 16:15:37 +0100

    can do multiple moves (somewhat)

--- core.py
+++ core.py
@@ -14,21 +14,44 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption('Python Storytelling')
 
+# emoji
 emoji = Emoji()
 test = pygame.sprite.RenderPlain(emoji)
 
+# bg
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0, 0, 0))
 
+# clock
+clock = pygame.time.Clock()
+
+# time
+font = pygame.font.Font(None, 50)
+text = font.render("Hello World", 1, (255, 255, 255))
+textpos = text.get_rect()
+textpos.centerx = background.get_rect().centerx
+print(textpos)
+
 screen.blit(background, (0, 0))
 
 while 1:
+    delta = clock.tick(60)
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()    
+        if event.type == pygame.QUIT: sys.exit()
 
-    emoji.update()
-
+    screen.blit(background, text.get_rect(), text.get_rect())
     screen.blit(background, emoji.rect, emoji.rect)
+    
+    emoji.update(delta)
+
+    text = font.render(str(pygame.time.get_ticks() / 1000) + ": " + str(emoji.rect.x) + ","  + str(emoji.rect.y), 1, (255, 255, 255))
+    screen.blit(text, text.get_rect())
+
+    #screen.blit(background, emoji.rect, emoji.rect)
+    screen.blit(background, emoji.rect, emoji.rect)
+    #screen.blit(background, text, text.get_rect())
     test.draw(screen)
+
     pygame.display.flip()
--- setup/emoji.py
+++ setup/emoji.py
@@ -17,9 +17,11 @@ class Emoji(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         #screen = pygame.display.get_surface()
 
-        self.addMove(0, 2, 1)
+        self.addMove(0, 200, 100)
         print(self.getVelForMove(self.getCurrentMove()))
-        self.addMove(3, 1, 1)
+        self.vel = self.getVelForMove(self.getCurrentMove())
+        self.addMove(50, 50, 100)
+        self.addMove(300, 400, 100)
 
     def getCurrentMove(self):
         return self.moves[self.moveIndex]
@@ -30,10 +32,40 @@ class Emoji(pygame.sprite.Sprite):
         vel = ((move[0] - self.rect.x) / move[2], (move[1] - self.rect.y) / move[2])
         return vel
 
-    def update(self):
-        # called once per frame
-        self.rect.x += self.vel[0]
-        self.rect.y += self.vel[1]
+    # called once per frame
+    def update(self, delta):
+        if not self.finishedMoving():
+            #print(self.rect)
+            move = self.getCurrentMove()
+            # area to test if emoji is in
+            TEST_LEN = 2
+            testRect = pygame.Rect(max(0, move[0] - TEST_LEN), move[1] - TEST_LEN, move[0] + TEST_LEN, move[1] + TEST_LEN)
+
+            #print("-> " + str(testRect))
+
+            if testRect.colliderect(self.rect):
+                self.vel = (0, 0)
+                self.rect.x = move[0]
+                self.rect.y = move[1]
+                self.incMoveIndex()
+                if not self.finishedMoving():
+                    print(str(self.moveIndex) + " / " + str(len(self.moves)))
+                    self.vel = self.getVelForMove(self.getCurrentMove())
+
+            self.addVel(self.vel * delta)
+
+    def incMoveIndex(self):
+        if self.moveIndex > len(self.moves) - 2:
+            self.moveIndex = -1
+        else:
+            self.moveIndex += 1
+
+    def finishedMoving(self):
+        return self.moveIndex == -1
+
+    def addVel(self, vel):
+        self.rect.x += vel[0]
+        self.rect.y += vel[1]
 
     def addMove(self, x, y, time):
         if len(self.moves) < self.MAX_MOVES:
