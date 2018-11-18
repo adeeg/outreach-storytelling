f2cb71641e98e4ab98df8a18538f961554ea111a
Nenivar <nenivar@gmail.com>
2018-11-18 15:08:14 +0000

    colour constants

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from internal.drawable.emoji import Emoji
 from util.observer import Observer, Event
 from util.timer import Timer
 from util.vector2 import Vector2
-from util.config import getConfig
+from util.config import getConfig, COL_WHITE, COL_BLACK, COL_RED, COL_ORANGE, COL_YELLOW, COL_BLUE, COL_GREEN
 
 pygame.init()
 screen = pygame.display.set_mode(tuple(getConfig('window_size')))
@@ -76,14 +76,14 @@ class Game(Observer):
 g = Game()
 
 newScene = SceneAction()
-newScene.setBackgroundColour((255, 255, 255))
+newScene.setBackgroundColour(COL_RED)
 
 newEmoji = Emoji('laughing')
 newScene.addEmoji(newEmoji)
 newEmoji2 = Emoji('shocked')
 newScene.addEmoji(newEmoji2)
 
-newScene.addAction(ActionText(5000, 'This is some text!'))
+newScene.addAction(ActionText(1000, 'This is some text!'))
 newScene.addAction(ActionMove(1000, newEmoji, Vector2(100, 150)))
 newScene.addAction(ActionScale(1000, newEmoji, 2))
 newScene.addAction(ActionWait(1000))
--- app/util/config.py
+++ app/util/config.py
@@ -1,5 +1,13 @@
 import json
 
+COL_BLACK   = (0,       0,      0)
+COL_WHITE   = (255,     255,    255)
+COL_RED     = (255,     179,    186)
+COL_BLUE    = (186,     225,    255)
+COL_GREEN   = (186,     255,    201)
+COL_ORANGE  = (255,     223,    186)
+COL_YELLOW  = (255,     255,    186)
+
 #TODO: not loading every time
 
 def getConfig(key):
